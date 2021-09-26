0ff47d0a0a9fb133c19865d2becf7acb012aadd2
Tupi Guedes <tupiguedes@outlook.com>
2021-09-26 13:49:43 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -31,4 +31,16 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		Optional<User> entity = repository.findById(id);
+		updateData(entity.get(), obj);
+		return repository.save(entity.get());
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
